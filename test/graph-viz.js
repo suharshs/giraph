47f8ca4118d4f0ffaba7f6bc7948fc6b82c0386e
suharshs <suharshs@gmail.com>
2013-04-13 01:51:54 -0500

    played with bonsai and fingure out how to draw graph, just need to add the drawing data to the graph objects decide upon where the visualization functions should be organized in the library

--- test/graph-viz.js
+++ test/graph-viz.js
@@ -1,4 +1,10 @@
-var v = new Circle(51, 51, 50)
+var edge = new Path()
+    .stroke('rgba(0,0,220,0.8)', 4)
+    .moveTo(50,50)
+    .lineTo(200,50)
+    .addTo(stage);
+
+var v = new Circle(50, 50, 25)
     .stroke('rgb(220,0,0)', 2)
     .fill('rgba(220,0,0,0.3)')
     .on("multi:drag", function(e){
@@ -9,6 +15,7 @@ var v = new Circle(51, 51, 50)
             x: e.x,
             y: e.y
         });
+        redrawEdge(edge, e.x, e.y, 200,50);
     })
     .on("multi:pointerup", function(e){
         this.animate( '300ms', {
@@ -16,3 +23,26 @@ var v = new Circle(51, 51, 50)
         });
     })
     .addTo(stage);
+
+var u = new Circle(200, 50, 25)
+    .stroke('rgb(0,220,0)', 2)
+    .fill('rgba(0,220,0,0.3)')
+    .on("multi:drag", function(e){
+        this.animate( '300ms', {
+            fillColor: 'rgba(0,220,0,0.6)'
+        });
+        this.attr({
+            x: e.x,
+            y: e.y
+        });
+    })
+    .on("multi:pointerup", function(e){
+        this.animate( '300ms', {
+            fillColor: 'rgba(0,220,0,0.3)'
+        });
+    })
+    .addTo(stage);
+
+function redrawEdge(edge,sx,sy,ex,ey){
+    edge.clear().moveTo(sx,sy).lineTo(ex,ey).addTo(stage);
+}
